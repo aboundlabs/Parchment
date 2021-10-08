17dc9664c7c5b7a40f3023d41ed48e833fbbf1fd
Tim Field <nobbis@gmail.com>
2021-10-07 19:38:17 -0700

    make PageViewController.layoutViews public

--- Parchment/Classes/PageViewController.swift
+++ Parchment/Classes/PageViewController.swift
@@ -302,7 +302,7 @@ extension PageViewController: PageViewManagerDelegate {
         scrollView.frame = view.bounds
     }
 
-    func layoutViews(for viewControllers: [UIViewController], keepContentOffset: Bool) {
+    public func layoutViews(for viewControllers: [UIViewController], keepContentOffset: Bool) {
         let viewControllers = isRightToLeft ? viewControllers.reversed() : viewControllers
 
         // Need to trigger a layout here to ensure that the scroll view
